5971143c27cf1da3d7fa331e1b6679d9fa760fc8
goreleaserbot <goreleaser@carlosbecker.com>
2018-06-19 14:46:10 -0400

    Brew formula update for predix-insights-cli version v1.0.15

--- Formula/predix-insights-cli.rb
+++ Formula/predix-insights-cli.rb
@@ -1,16 +1,16 @@
 class PredixInsightsCli < Formula
   desc "PI is a CLI library for Predix Insights. This top technology lets you concentrate on building analytical pipelines, not managing infrastructure."
   homepage "https://www.predix.io/"
-  url "https://github.com/scottmcclary1/predix-insights-cli/releases/download/v1.0.14/predix-insights-cli_1.0.14_macOS_64-bit.tar.gz"
-  version "1.0.14"
-  sha256 "e52c61a42725fae6ddc6db8cf88904b7c9e465e2351c618d0f62effb665d46de"
+  url "https://github.com/scottmcclary1/predix-insights-cli/releases/download/v1.0.15/predix-insights-cli_1.0.15_macOS_64-bit.tar.gz"
+  version "1.0.15"
+  sha256 "74354e7d8bfa670fdf4040a40f27de0ed4c4b994bdd53e448da8be616fba26ef"
   
   depends_on "bash-completion"
 
   def install
     bin.install "pi"
-    mkdir_p HOMEBREW_PREFIX"/etc/bash_completion.d"
-    mv prefix"/scripts/pi_completion.sh", HOMEBREW_PREFIX"/etc/bash_completion.d/pi_completion.sh"
+    mkdir_p "#{HOMEBREW_PREFIX}/etc/bash_completion.d"
+    mv "#{prefix}/scripts/pi_completion.sh", "#{HOMEBREW_PREFIX}/etc/bash_completion.d/pi_completion.sh"
     echo "if [ -f #{HOMEBREW_PREFIX} ]; then . #{HOMEBREW_PREFIX}/etc/bash_completion; fi" >> ~/.bashrc
     source ~/.bashrc
   end
