46414864e8f89d6d06e0978a083635cc0f2ee68e
goreleaserbot <goreleaser@carlosbecker.com>
2018-06-19 16:02:32 -0400

    Brew formula update for predix-insights-cli version v1.0.18

--- Formula/predix-insights-cli.rb
+++ Formula/predix-insights-cli.rb
@@ -1,19 +1,22 @@
 class PredixInsightsCli < Formula
   desc "PI is a CLI library for Predix Insights. This top technology lets you concentrate on building analytical pipelines, not managing infrastructure."
   homepage "https://www.predix.io/"
-  url "https://github.com/scottmcclary1/predix-insights-cli/releases/download/v1.0.17/predix-insights-cli_1.0.17_macOS_64-bit.tar.gz"
-  version "1.0.17"
-  sha256 "36c9a40021c4d1884604b449c942f07616df0d40386f342d5b86b2c0d63d953b"
+  url "https://github.com/scottmcclary1/predix-insights-cli/releases/download/v1.0.18/predix-insights-cli_1.0.18_macOS_64-bit.tar.gz"
+  version "1.0.18"
+  sha256 "c4081e7168fb4e7c3117389c9160e05b260e0f17a66dc774af462e2643b234af"
   
   depends_on "bash-completion"
 
   def install
     bin.install "pi"
     mkdir_p "#{HOMEBREW_PREFIX}/etc/bash_completion.d"
-    pwd
     cp "scripts/pi_completion.sh", "#{HOMEBREW_PREFIX}/etc/bash_completion.d"
-    echo "if [ -f #{HOMEBREW_PREFIX} ]; then . #{HOMEBREW_PREFIX}/etc/bash_completion; fi" >> ~/.bashrc
+    system "echo 'if [ -f $(brew --prefix)/etc/bash_completion ]; then . $(brew --prefix)/etc/bash_completion; fi' >> ~/.bashrc"
+  end
+
+  def caveats; <<~EOS
     source ~/.bashrc
+  EOS
   end
 
   test do
