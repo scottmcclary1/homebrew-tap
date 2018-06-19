e62dfe089ce0b0cc832f712d4cee9a93a90bd55c
goreleaserbot <goreleaser@carlosbecker.com>
2018-06-19 16:35:49 -0400

    Brew formula update for predix-insights-cli version v1.0.20

--- Formula/predix-insights-cli.rb
+++ Formula/predix-insights-cli.rb
@@ -1,9 +1,9 @@
 class PredixInsightsCli < Formula
   desc "PI is a CLI library for Predix Insights. This top technology lets you concentrate on building analytical pipelines, not managing infrastructure."
   homepage "https://www.predix.io/"
-  url "https://github.com/scottmcclary1/predix-insights-cli/releases/download/v1.0.19/predix-insights-cli_1.0.19_macOS_64-bit.tar.gz"
-  version "1.0.19"
-  sha256 "af244cc5519b8305665eb84902f6e99ef0f0796046e3f0086ad5e8b481864026"
+  url "https://github.com/scottmcclary1/predix-insights-cli/releases/download/v1.0.20/predix-insights-cli_1.0.20_macOS_64-bit.tar.gz"
+  version "1.0.20"
+  sha256 "110df130d7ca31896c86ff6a4af8f4f2a87c5c88c5c4cf8f4c6768897808348d"
   
   depends_on "bash-completion"
 
@@ -15,7 +15,7 @@ class PredixInsightsCli < Formula
 
   def caveats; <<~EOS
     Run the following command or open a new terminal tab/window:
-    	source ~/.bash_profile
+      source ~/.bash_profile
   EOS
   end
 
