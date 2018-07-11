710071fbcd86706d153a4ab762180bbedfd388b6
goreleaserbot <goreleaser@carlosbecker.com>
2018-07-11 14:28:22 -0700

    Brew formula update for predix-insights-cli version v1.0.21

--- Formula/predix-insights-cli.rb
+++ Formula/predix-insights-cli.rb
@@ -1,9 +1,9 @@
 class PredixInsightsCli < Formula
   desc "PI is a CLI library for Predix Insights. This top technology lets you concentrate on building analytical pipelines, not managing infrastructure."
   homepage "https://www.predix.io/"
-  url "https://github.com/scottmcclary1/predix-insights-cli/releases/download/v1.0.20/predix-insights-cli_1.0.20_macOS_64-bit.tar.gz"
-  version "1.0.20"
-  sha256 "110df130d7ca31896c86ff6a4af8f4f2a87c5c88c5c4cf8f4c6768897808348d"
+  url "https://github.com/scottmcclary1/predix-insights-cli/releases/download/v1.0.21/predix-insights-cli_1.0.21_macOS_64-bit.tar.gz"
+  version "1.0.21"
+  sha256 "67dbf3d0f5580e030d40302cc4fde7e5e04397d755c4ffb4a9793a632691aa90"
   
   depends_on "bash-completion"
 
